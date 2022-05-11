e87e6d2eedb4eee7efae124026db69c7fd899a05
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2022-05-11 23:48:47 +0700

    Fix error when build,

--- proto/gen.sh
+++ proto/gen.sh
@@ -1,2 +1,5 @@
-protoc *.proto --go_out="../server"
+# Create grpc
+protoc *.proto --go_out="../server" --go-grpc_out="../server"
+
+#Create grpc gateway
 protoc --grpc-gateway_out="logtostderr=true:../server" *.proto
